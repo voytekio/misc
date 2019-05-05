281618842f38edbbaef82c5ca85311b262e83c9d
voytekio <voytek@voytek.io>
2019-05-05 14:50:28 -0400

    add simple check and remediate script for fixing network disconnects

---
+++ check_and_remediate.py
@@ -0,0 +1,62 @@
+#!/usr/bin/python
+from __future__ import print_function
+from subprocess import Popen, PIPE
+import json
+import logging
+import pdb
+import shlex
+import sys
+
+def run_cmd(cmd, returnstring):
+    log.debug('cmd is: {0}'.format(cmd))
+    log.debug('returnstring is: {0}'.format(returnstring))
+    cmdplus = shlex.split(cmd)
+    try:
+        process = Popen(cmdplus, stdout=PIPE)
+        cmdoutput = process.communicate()
+    except OSError:
+        log.debug('OS Exception occurred')
+        return 0
+    exitcode = process.wait()
+    log.debug('cmd output is: {0}'.format(cmdoutput))
+    log.debug('exitcode is: {0}'.format(exitcode))
+    #pdb.set_trace()
+    if returnstring in cmdoutput[0]:
+        return 1
+    else:
+        return 0
+
+def main():
+    pdb.set_trace()
+    with open(sys.argv[1]) as f:
+        config_json = json.load(f)
+    formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s: %(message)s',datefmt='%Y-%m-%d %I:%M:%S %p')
+    formatter_no_source = logging.Formatter('%(asctime)s %(levelname)s: %(message)s',datefmt='%Y-%m-%d %I:%M:%S %p')
+    ch = logging.StreamHandler() # console handler
+    ch.setLevel(logging.INFO)
+    ch.setFormatter(formatter_no_source)
+    fl = logging.FileHandler(config_json['logname']) #file handler
+    fl.setLevel(logging.INFO)
+    fl.setFormatter(formatter)
+    log = logging.getLogger()
+    log.setLevel(logging.DEBUG)
+    log.addHandler(ch)
+    log.addHandler(fl)
+
+
+    check_command_syntax = config_json['check_command_syntax']
+    check_command_result = config_json['check_command_result']
+    remediation_command_syntax = config_json['remediation_command_syntax']
+    remediation_command_result = config_json['remediation_command_result']
+
+    if run_cmd(check_command_syntax, check_command_result):
+        log.info('check command success')
+    else:
+        log.warn('check command failed. Running remediation command.')
+        if run_cmd(remediation_command_syntax, remediation_command_result):
+            log.info('remediation command success')
+        else:
+            log.error('error running remediation command')
+
+if __name__ == "__main__":
+    main()
